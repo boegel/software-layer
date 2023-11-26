d6bf254c3d669286854b22eab269a5ea3115c4ae
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 15:34:48 +0100

    print list of directories in software layer in test script

--- tests/EasyBuild/test-eb.sh
+++ tests/EasyBuild/test-eb.sh
@@ -7,4 +7,6 @@ source /cvmfs/software.eessi.io/versions/${EESSI_VERSION}/init/bash
 module load EasyBuild
 eb --version
 
+find /cvmfs/software.eessi.io/versions/${EESSI_VERSION}/software/linux/$(uname -m) -type d
+
 echo "All checks for test phase PASSED"
