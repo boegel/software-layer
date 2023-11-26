a216ee5254f18e0f818fa54b23cea5f3b4e8f36b
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 16:07:50 +0100

    fix passing resume option to eessi_container.sh

--- bot/test.sh
+++ bot/test.sh
@@ -5,11 +5,11 @@ source scripts/cfg_files.sh
 
 job_dir=${PWD}
 job_out="slurm-${SLURM_JOB_ID}.out"
-resume_opt=$(grep "to resume session add" ${job_dir}/${job_out} | head -1 | sed "s@.*'\(--resume [^']*\)'.*@\1@g")
+resume_dir=$(grep "to resume session add" ${job_dir}/${job_out} | head -1 | sed "s@.*'--resume \([^']*\)'.*@\1@g")
 
 cfg_load cfg/job.cfg
 export EESSI_SOFTWARE_SUBDIR_OVERRIDE=$(cfg_get_value "architecture" "software_subdir")
 
 export EESSI_REPOS_CFG_DIR_OVERRIDE=${HOME}/bot-configs/mc-aws-rocky88-202310/repos
 
-./eessi_container.sh --verbose --access rw --mode run --container docker://ghcr.io/eessi/build-node:debian11 --repository eessi.io-2023.06-software --storage /tmp/${USER}/EESSI ${resume_opt} tests/EasyBuild/test-eb.sh
+./eessi_container.sh --verbose --access rw --mode run --container docker://ghcr.io/eessi/build-node:debian11 --repository eessi.io-2023.06-software --storage /tmp/${USER}/EESSI --resume ${resume_dir} tests/EasyBuild/test-eb.sh
