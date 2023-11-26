913cc2ec9b81ecb288c8a67dd9f800a15f832ec5
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 14:31:17 +0100

    fix bot/check-result.sh

--- bot/check-test.sh
+++ bot/check-test.sh
@@ -7,15 +7,17 @@ job_test_result_file=_bot_job${SLURM_JOB_ID}.test
 grep_out=$(grep '^All checks PASSED' ${job_dir}/${job_out})
 all_checks_passed=$?
 
-if [[ ${all_checks_passed}} -eq 0 ]]; then
+if [[ ${all_checks_passed} -eq 0 ]]; then
+    comment_description="all checks pass in test phase"
     status="SUCCESS"
 else
+    comment_description="some checks FAILED in test phase"
     status="FAILURE"
 fi
 
 echo "[RESULT]" > ${job_test_result_file}
 echo -n "comment_description = " >> ${job_test_result_file}
-echo "all checks pass for test phase" >> ${job_test_result_file}
+echo "${comment_description}" >> ${job_test_result_file}
 echo "status = ${status}" >> ${job_test_result_file}
 
 test "${status}" == "SUCCESS"
