3823ed972541d7e94f6e8ec1241aa45790fa4ba5
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 15:29:08 +0100

    correctly set $EESSI_SOFTWARE_SUBDIR_OVERRIDE in bot/test.sh

--- bot/test.sh
+++ bot/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+source scripts/utils.sh
+source scripts/cfg_files.sh
+
 job_dir=${PWD}
 job_out="slurm-${SLURM_JOB_ID}.out"
 resume_opt=$(grep "to resume session add" ${job_dir}/${job_out} | head -1 | sed "s@.*'\(--resume [^']*\)'.*@\1@g")
+
+cfg_load cfg/job.cfg
+export EESSI_SOFTWARE_SUBDIR_OVERRIDE=$(cfg_get_value "architecture" "software_subdir")
 
 export EESSI_REPOS_CFG_DIR_OVERRIDE=${HOME}/bot-configs/mc-aws-rocky88-202310/repos
 
