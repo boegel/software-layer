702ee08ec7912ea2620e30df576d9f8b32aa8964
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 14:49:34 +0100

    set $EESSI_REPOS_CFG_DIR in bot/test.sh

--- bot/test.sh
+++ bot/test.sh
@@ -4,4 +4,6 @@ job_dir=${PWD}
 job_out="slurm-${SLURM_JOB_ID}.out"
 resume_opt=$(grep "to resume session add" ${job_dir}/${job_out} | head -1 | sed "s@.*'\(--resume [^']*\)'.*@\1@g")
 
+export EESSI_REPOS_CFG_DIR=${HOME}/bot-configs/mc-aws-rocky88-202310/repos
+
 ./eessi_container.sh --verbose --access rw --mode run --container docker://ghcr.io/eessi/build-node:debian11 --repository eessi.io-2023.06-software --storage /tmp/${USER}/EESSI ${resume_opt} tests/EasyBuild/test-eb.sh
